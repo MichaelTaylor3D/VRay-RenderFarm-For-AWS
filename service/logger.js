5230af558eec8dd7aa8bba87ab98c723942d0dd9
Michael Taylor <mtaylor@michaeltaylor3d.com>
2017-11-24 14:12:46 -0500

    Simple winston format:

--- service/logger.js
+++ service/logger.js
@@ -1,11 +1,11 @@
 const winston = require('winston');
 
 const logger = winston.createLogger({
-  format: winston.format.json(),
+  format: winston.format.simple(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'log/combined.log' })
+    new winston.transports.File({ filename: './log/error.log', level: 'error' }),
+    new winston.transports.File({ filename: './log/combined.log' })
   ]
 });
 
