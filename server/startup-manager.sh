d1ce3fc685d37a2fe3b1095b6be27576a4009474
Michael.Taylor <mtaylor@michaeltaylor3d.com>
2020-05-12 19:00:24 -0400

    Fix paths

--- server/startup-manager.sh
+++ server/startup-manager.sh
@@ -1,9 +1,9 @@
-cd /home/ec2-user/oonix-render-farm && /usr/bin/git fetch --all && /usr/bin/git reset --hard origin/master
-cd /home/ec2-user/oonix-render-farm && /usr/bin/npm install
-cd /home/ec2-user/oonix-render-farm/client && /usr/bin/npm run build
+cd /home/ec2-user/render-farm && /usr/bin/git fetch --all && /usr/bin/git reset --hard origin/master
+cd /home/ec2-user/render-farm && /usr/bin/npm install
+cd /home/ec2-user/render-farm/client && /usr/bin/npm run build
 
 tmux \
-    new-session -d -s renderfarm "cd /home/ec2-user/oonix-render-farm/service && /usr/bin/node . ; read" \; \
-    split-window "cd /home/ec2-user/oonix-render-farm/server && /usr/bin/node . ; read" \; \
+    new-session -d -s renderfarm "cd /home/ec2-user/render-farm/service && /usr/bin/node . ; read" \; \
+    split-window "cd /home/ec2-user/render-farm/server && /usr/bin/node . ; read" \; \
     split-window \; \
     select-layout even-horizontal
--- server/startup-render-node.sh
+++ server/startup-render-node.sh
@@ -1,7 +1,7 @@
-cd /home/ec2-user/oonix-render-farm && /usr/bin/git fetch --all && /usr/bin/git reset --hard origin/master
-cd /home/ec2-user/oonix-render-farm && /usr/bin/npm install
+cd /home/ec2-user/render-farm && /usr/bin/git fetch --all && /usr/bin/git reset --hard origin/master
+cd /home/ec2-user/render-farm && /usr/bin/npm install
 
 tmux \
-    new-session -d -s renderfarm "cd /home/ec2-user/oonix-render-farm/service/render-node && /usr/bin/node . ; read" \; \
+    new-session -d -s renderfarm "cd /home/ec2-user/render-farm/service/render-node && /usr/bin/node . ; read" \; \
     split-window \; \
     select-layout even-horizontal
